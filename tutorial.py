7beca0ca8f5cab646a73f252bf23b6b7f1f84674
DanielJAM <dmt700@vu.nl>
2015-09-30 23:45:39 +0200

    Update tutorial.py
    line 35    reasoning = request.args.get('reasoning', None)
    line 51        if reasoning =='true':

--- tutorial.py
+++ tutorial.py
@@ -32,7 +32,7 @@ def sparql():
 
     endpoint = request.args.get('endpoint', None)
     query = request.args.get('query', None)
-    reasoning = request.args.get('reasoning', None)
+    reasoning = request.args.get('reasoning', None)  #HERE
 
     return_format = request.args.get('format','JSON')
 
@@ -48,7 +48,7 @@ def sparql():
             sparql.setReturnFormat(JSON)
             sparql.addParameter('Accept','application/sparql-results+json')
 
-        if reasoning =='true':
+        if reasoning =='true':  #HERE
             sparql.addParameter('reasoning','true')
 
         app.logger.debug('Query:\n{}'.format(query))
